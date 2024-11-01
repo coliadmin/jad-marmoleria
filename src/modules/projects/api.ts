883764f359d1976748dc5da3ed3873adeebe77f5
pufi <thomas.r.pufahl@gmail.com>
2024-10-31 21:16:39 -0300

    fix import

--- src/modules/projects/api.ts
+++ src/modules/projects/api.ts
@@ -1,9 +1,13 @@
-import {Api} from "@/types";
+import type {Project} from "@/modules/projects";
+
+import {Api} from "@/lib/strapi";
 
 async function getProjects() {}
 
 export const api: Api<Project> = {
-  get: getProjects,
+  get: () => {
+    throw new Error("Not implemented");
+  },
   fetch: () => {
     throw new Error("Not implemented");
   },
