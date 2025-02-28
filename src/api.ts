d42e1f27725f51d3c15855a77e3eab7953a76238
Thomas Rodolfo Pufahl <trpufahl@suppliers.tenaris.com>
2025-02-28 20:58:32 -0300

    Add projects API

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,12 @@
 import {api as products} from "@/modules/product";
-import {api as colors} from "@/modules/color";
-import {api as uses} from "@/modules/use";
-import {api as materials} from "@/modules/material";
+import {api as colors} from "@/modules/categories/color";
+import {api as uses} from "@/modules/categories/use";
+import {api as materials} from "@/modules/categories/material";
+import {api as projects} from "@/modules/projects";
 
 export const api = {
   products,
+  projects,
   colors,
   uses,
   materials,
