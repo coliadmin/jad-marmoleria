7672f1cd444d21ca453a44710bc02e89491a8444
Astrid <astridnaivirt22@outlook.com>
2025-02-28 10:02:00 -0300

    Update product type and API to include 'usos' field and adjust related components

--- src/modules/product/types.ts
+++ src/modules/product/types.ts
@@ -1,11 +1,13 @@
 import type {Data, Image} from "@/lib/strapi";
 
+import {Use} from "../use";
+
 export interface ProductDTO {
   nombre: string;
   slug: string;
   descripcion: string | null;
   espesor: string | null;
-  uso: string | null;
+  usos: Use[] | null;
   disponibilidad: boolean;
   portada: Image;
   imagenes: Image[];
