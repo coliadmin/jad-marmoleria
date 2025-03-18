d90e06ee10a34915681d48440aa31e2941e4bac5
thomaspufahl <thomas.pufahl@alumnos.frgp.utn.edu.ar>
2025-03-18 17:37:43 -0300

    Add  functions getFileExtension and isImageOrVideo to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,5 @@
+import * as path from "path";
+
 import {clsx, type ClassValue} from "clsx";
 import {twMerge} from "tailwind-merge";
 
@@ -7,4 +9,23 @@ export function cn(...inputs: ClassValue[]) {
 
 export function capitalize(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function getFileExtension(filePath: string): string {
+  return path.extname(filePath).toLowerCase();
+}
+
+export function isImageOrVideo(filePath: string): "image" | "video" | null {
+  const imageExtensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp"];
+  const videoExtensions = [".mp4", ".avi", ".mov", ".mkv", ".webm"];
+
+  const ext = getFileExtension(filePath);
+
+  if (imageExtensions.includes(ext)) {
+    return "image";
+  } else if (videoExtensions.includes(ext)) {
+    return "video";
+  } else {
+    return null;
+  }
 }
