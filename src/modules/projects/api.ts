c6e3f2515c999f4eb9fb389d68da1c3f21b1421d
thomaspufahl <thomas.pufahl@alumnos.frgp.utn.edu.ar>
2025-03-18 17:42:00 -0300

    Add fetchProject

--- src/modules/projects/api.ts
+++ src/modules/projects/api.ts
@@ -7,12 +7,19 @@ export async function getProjects(): QueryResponse<Project[]> {
     "projects?populate[imagenes][fields][0]=name&populate[imagenes][fields][1]=url&populate[imagenes][fields][2]=hash",
   );
 
-  return res;
+export async function fetchProject(slug: string): Promise<Project | null> {
+  try {
+    const {data} = await query<Project[]>(
+      `projects?filters[slug][$contains]=${slug}&populate[imagenes][fields][0]=name&populate[imagenes][fields][1]=url&populate[imagenes][fields][2]=hash&populate[portada][fields][0]=name&populate[portada][fields][1]=url&populate[portada][fields][2]=hash`,
+    );
+
+    return data[0];
+  } catch (error) {
+    throw error;
+  }
 }
 
 export const api: Api<Project> = {
   get: getProjects,
-  fetch: () => {
-    throw new Error("Not implemented");
-  },
+  fetch: fetchProject,
 };
