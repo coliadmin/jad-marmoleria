270c5b376e9713d1cb93b60e5bdd3628ab5a7fa9
thomaspufahl <thomas.pufahl@alumnos.frgp.utn.edu.ar>
2025-03-18 17:42:26 -0300

    Add full url to multimedia

--- src/modules/projects/api.ts
+++ src/modules/projects/api.ts
@@ -1,11 +1,26 @@
 import type {Project} from "@/modules/projects";
 
-import {Api, query, QueryResponse} from "@/lib/strapi";
+import {Api, query, QueryResponse, toUrl} from "@/lib/strapi";
+import {STRAPI_HOST} from "@/config";
 
 export async function getProjects(): QueryResponse<Project[]> {
   const res = await query<Project[]>(
-    "projects?populate[imagenes][fields][0]=name&populate[imagenes][fields][1]=url&populate[imagenes][fields][2]=hash",
+    "projects?populate[imagenes][fields][0]=name&populate[imagenes][fields][1]=url&populate[imagenes][fields][2]=hash&populate[videos][fields][0]=name&populate[videos][fields][1]=url&populate[videos][fields][2]=hash&populate[portada][fields][0]=name&populate[portada][fields][1]=url&populate[portada][fields][2]=hash",
   );
+
+  const cpy = res;
+
+  res.data.forEach((x, idx) => {
+    cpy.data.splice(idx, 1, {
+      ...x,
+      portada: {...x.portada, url: STRAPI_HOST + x.portada.url},
+      imagenes: x.imagenes.map((x) => ({...x, url: STRAPI_HOST + x.url})),
+      videos: x.videos.map((x) => ({...x, url: STRAPI_HOST + x.url})),
+    });
+  });
+
+  return cpy;
+}
 
 export async function fetchProject(slug: string): Promise<Project | null> {
   try {
