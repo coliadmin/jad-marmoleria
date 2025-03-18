7a84143a43b81ca6de7da790b6aff77be1d0dc92
thomaspufahl <thomas.pufahl@alumnos.frgp.utn.edu.ar>
2025-03-18 17:35:34 -0300

    Refactor ProjectsPage

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,26 @@
-import {H2} from "@/components/typo";
+import {api} from "@/api";
+import {H3} from "@/components/typo";
+import {ProductLink} from "@/modules/product";
+import {ProjectArticle} from "@/modules/projects/components/project-article";
 
 export default async function ProjectsPage() {
+  const {data} = await api.projects.get();
+
   return (
-    <section className="container flex h-full border-e border-s">
-      <header className="w-full pb-4 pt-8">
-        <H2 className="border-none text-center">Our Work</H2>
-      </header>
+    <section className="container flex border-e border-s">
+      <section className="flex-1 border-s">
+        <header className="w-full pb-2">
+          <H3 className="border-b py-3 text-center">Our Work</H3>
+        </header>
+        <ul className="grid grid-cols-3 gap-10 py-8">
+          {data.map((x) => (
+            <li key={x.id} className="inline-flex max-w-[19rem] justify-self-center ">
+              {/* <ProjectArticle project={x}> */}
+              <ProductLink path="projects" product={x} ratio={1} />
+            </li>
+          ))}
+        </ul>
+      </section>
     </section>
   );
 }
