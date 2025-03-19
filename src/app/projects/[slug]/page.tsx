db817108408a77ec5f090a9e30bce68b1e6161db
Astrid <astridnaivirt22@outlook.com>
2025-03-19 16:11:53 -0300

    Add props to ProjectPage, generateStaticParams and generateMetaData

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -6,8 +6,35 @@ import {ProjectArticle} from "@/modules/projects/components/project-article";
 import {VerticalCarousel} from "@/components/vertical-carousel";
 import {Skeleton} from "@/components/ui/skeleton";
 
-export default async function ProjectPage() {
+type ProjectPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateStaticParams() {
   const {data} = await api.projects.get();
+
+  return data.map((p) => ({
+    slug: p.slug,
+  }));
+}
+
+export async function generateMetadata({params: {slug}}: ProjectPageProps) {
+  const project = await api.projects.fetch(slug);
+
+  if (!project) {
+    return notFound();
+  }
+
+  return {
+    title: project.nombre,
+    description: project.descripcion,
+    image: project.portada.url,
+  };
+}
+
+export default async function ProjectPage({params: {slug}}: ProjectPageProps) {
 
   return (
     <section className="container">
