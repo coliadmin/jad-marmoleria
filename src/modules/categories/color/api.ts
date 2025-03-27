1934939d6d231f03cdf7ccadd8a5cecbc065222d
Astrid <astridnaivirt22@outlook.com>
2025-03-27 12:16:05 -0300

    Add caching tags to colors query

--- src/modules/categories/color/api.ts
+++ src/modules/categories/color/api.ts
@@ -3,7 +3,7 @@ import {Color} from "./types";
 import {Api, query, QueryResponse} from "@/lib/strapi";
 
 async function getColors(): QueryResponse<Color[]> {
-  const res = await query<Color[]>("colors?populate[fields][0]=nombre&populate[fields][1]=slug");
+  const res = await query<Color[]>("colors?populate[fields][0]=nombre&populate[fields][1]=slug", { next: { tags: ['color'] } });
 
   return res;
 }
