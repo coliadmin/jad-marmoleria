940f92b6328b7a0583ec3eca52e7cdca401ad80c
astridn22 <astridnaivirt22@outlook.com>
2025-04-14 16:20:15 -0300

    Upd whatsapp api

--- src/modules/content/whatsapp/api.ts
+++ src/modules/content/whatsapp/api.ts
@@ -3,7 +3,9 @@ import {WhatsApp} from "./types";
 import {QueryResponse, query} from "@/lib/strapi";
 
 export async function getWhatsApp(): QueryResponse<WhatsApp> {
-  const res = await query<WhatsApp>("whatsapp");
+  const res = await query<WhatsApp>("whatsapp", {
+    next: {tags: ["whatsapp"]},
+  });
 
   return res;
 }
