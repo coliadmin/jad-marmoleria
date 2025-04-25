c296a4b4b170381d7ff109dbb440175633f050bb
Astrid <astridnaivirt22@outlook.com>
2025-04-25 11:07:40 -0300

    Upd WhatsApp API

--- src/modules/content/whatsapp/api.ts
+++ src/modules/content/whatsapp/api.ts
@@ -1,11 +1,27 @@
-import {WhatsApp} from "./types";
+import {WhatsApp, WhatsAppDTO} from "./types";
 
 import {QueryResponse, query} from "@/lib/strapi";
 
-export async function getWhatsApp(): QueryResponse<WhatsApp> {
-  const res = await query<WhatsApp>("whatsapp", {
-    next: {tags: ["whatsapp"]},
-  });
+async function fetchWhatsApp(): QueryResponse<WhatsAppDTO | null> {
+  try {
+    const res = await query<WhatsAppDTO>("whatsapp", {
+      next: {tags: ["whatsapp"]},
+    });
 
-  return res;
+    return res;
+  } catch (error) {
+    throw error;
+  }
 }
+
+async function getWhatsApp(): Promise<WhatsApp> {
+  const {data} = await fetchWhatsApp();
+
+  const cpy = {...data, mensaje: data.mensaje ?? ""};
+
+  return cpy;
+}
+
+export const api = {
+  get: getWhatsApp,
+};
