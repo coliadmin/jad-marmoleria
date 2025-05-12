dbf94a16eb52acd971dcb1e19a1b19c327e4d7ab
astridn22 <astridnaivirt22@outlook.com>
2025-05-12 18:24:14 -0300

    upd projectspage and projectpage

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -16,7 +16,7 @@ export default async function ProjectsPage() {
   const projects = await api.projects.getList();
 
   return (
-    <section className="container flex lg:border-e lg:border-s">
+    <section className="container flex max-w-xl sm:max-w-2xl md:max-w-5xl lg:max-w-[88rem] lg:border-e lg:border-s">
       <section className="flex-1">
         <header className="w-full border-b pb-0">
           <H3 className="py-3 text-center">Our Work</H3>
