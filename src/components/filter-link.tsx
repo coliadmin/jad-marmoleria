6743579622c2c7c6521633c27b9e14dc75e677d2
pufi <thomas.r.pufahl@gmail.com>
2025-06-02 18:14:33 -0300

    Refactor: filterLink to add multiple filters on searchParams

--- src/components/filter-link.tsx
+++ src/components/filter-link.tsx
@@ -6,6 +6,7 @@ import {Color} from "@/modules/categories/color";
 import {Use} from "@/modules/categories/use";
 import {Material} from "@/modules/categories/material";
 import {cn} from "@/lib/utils";
+import { X } from "lucide-react";
 
 type FilterLinkProps = {
   category: string;
@@ -14,25 +15,43 @@ type FilterLinkProps = {
   accentColor?: string;
 };
 
-export function FilterLink({category, value, className, accentColor}: FilterLinkProps) {
+
+export function FilterLink({ category, value, className, accentColor }: FilterLinkProps) {
   const searchParams = useSearchParams();
-  const activeValue = searchParams.get("value");
-  accentColor = activeValue === value.slug ? accentColor : "transparent";
+  const activeValue = searchParams.get(category); 
+  const isCurrentFilterActive = activeValue === value.slug;
+
+  accentColor = isCurrentFilterActive ? accentColor : "transparent";
+
+  const currentSearchParams = new URLSearchParams(searchParams.toString());
+  let newHref: string;
+
+  if (isCurrentFilterActive) {
+    currentSearchParams.delete(category);
+  } else {
+    currentSearchParams.set(category, value.slug);
+  }
+
+  if (currentSearchParams.toString()) {
+    newHref = `?${currentSearchParams.toString()}`;
+  } else {
+    newHref = `/products`; 
+  }
 
   return (
     <Link
       prefetch
       className={cn(
         "rounded-full border px-4 py-1 lg:rounded-none lg:border-none lg:px-0 lg:py-0 lg:hover:underline",
-        activeValue === value.slug &&
+        isCurrentFilterActive &&
           "lg:rounded-e-full lg:rounded-s-none lg:border-none lg:px-2",
-           className
+        className
       )}
-      style={{backgroundColor: accentColor}}
-
-      href={`/products?category=${category.toString()}&value=${value.slug}`}
+      style={{ backgroundColor: accentColor }}
+      href={newHref} 
     >
       {value.nombre}
+      <X className={cn("hidden ml-1 size-3 opacity-65", isCurrentFilterActive && "inline-flex")} />
     </Link>
   );
 }
