f3c21e148fdf33967ae3afeafcdf47ec6f95a4e8
Santiago Perez Delfino <santiago@utriper.com>
2025-07-11 20:39:06 -0300

    Upd:  products link for zoom in images with new Modals component

--- src/modules/product/components/product-link.tsx
+++ src/modules/product/components/product-link.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import type {Product} from "@/modules/product";
 
 import {ChevronRight} from "lucide-react";
 import {Link} from "next-view-transitions";
+import {useRouter} from "next/navigation";
 
 import {H4} from "@/components/typo";
 import {VerticalImage} from "@/components/vertical-image";
+import {ImageModal} from "@/components/image-modal";
 import {cn} from "@/lib/utils";
 import {Project} from "@/modules/projects";
 
@@ -23,25 +27,39 @@ export function ProductLink({
   color,
   path = "products",
 }: ProductLinkProps) {
+  const router = useRouter();
+
+  const handleImageClick = () => {
+    router.push(`/${path}/${product.slug}`);
+  };
+
   return (
-    <Link prefetch className="group space-y-2" href={`/${path}/${product.slug}`}>
-      <VerticalImage
-        alt={product.portada.name}
-        className={cn("w-vertical border p-2 transition-all duration-200 ease-in-out group-hover:border-foreground group-hover:bg-slate-100",className)}
-        ratio={ratio}
+    <div className="group space-y-2">
+      <ImageModal
         src={product.portada.url}
-      />
-      <div className="inline-flex w-full items-center">
-        <H4
-          className={cn(
-            "inline-flex w-fit items-center text-wrap border-b text-lg transition-all duration-200 ease-in-out group-hover:border-foreground",
-            color,
-          )}
-        >
-          {product.nombre}
-        </H4>
-        <ChevronRight className="mb-icon ms-2 size-5 text-border transition-all duration-200 ease-in-out group-hover:inline-block group-hover:text-foreground" />
-      </div>
-    </Link>
+        alt={product.portada.name}
+        onImageClick={handleImageClick}
+      >
+        <VerticalImage
+          alt={product.portada.name}
+          className={cn("w-vertical border p-2 transition-all duration-200 ease-in-out group-hover:border-foreground group-hover:bg-slate-100 cursor-pointer",className)}
+          ratio={ratio}
+          src={product.portada.url}
+        />
+      </ImageModal>
+      <Link prefetch href={`/${path}/${product.slug}`}>
+        <div className="inline-flex w-full items-center">
+          <H4
+            className={cn(
+              "inline-flex w-fit items-center text-wrap border-b text-lg transition-all duration-200 ease-in-out group-hover:border-foreground",
+              color,
+            )}
+          >
+            {product.nombre}
+          </H4>
+          <ChevronRight className="mb-icon ms-2 size-5 text-border transition-all duration-200 ease-in-out group-hover:inline-block group-hover:text-foreground" />
+        </div>
+      </Link>
+    </div>
   );
 }
