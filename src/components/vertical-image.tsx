10e8371329cbd0cfa89f46409feb8c16a2de4ada
Santiago Perez Delfino <santiago@utriper.com>
2025-07-11 20:38:23 -0300

    Upd: update vertical-image for zoom in images with new Modals component

--- src/components/vertical-image.tsx
+++ src/components/vertical-image.tsx
@@ -15,7 +15,7 @@ export function VerticalImage({alt, src, className, ratio = 9 / 16}: VerticalIma
     <div className={cn("w-full p-2 md:w-vertical", className)}>
       <AspectRatio ratio={ratio}>
         {multimedia === "image" ? (
-          <img alt={alt} className="h-full w-full object-cover" src={src} />
+          <img alt={alt} className="h-full w-full object-cover cursor-pointer" src={src} />
         ) : (
           <video controls src={src}>
             <track default kind="captions" srcLang="es" />
